6def3f4475bc1b2c977f7b5d4058933e79dbcfab
Nick Endle <nick.endle@gmail.com>
2019-07-19 10:08:58 -0500

    Better handling of initial sold out options
    This immediately removes any values that aren't actually available as option group values, and marks values as Sold Out if there aren't any matching options in stock.

--- source/javascripts/product-option-groups.js
+++ source/javascripts/product-option-groups.js
@@ -1,22 +1,62 @@
-
 function processProduct(product) {
   if (product.has_option_groups) {
     disableAddButton();
-    if (product.option_groups.length == 1) {
+
+    if (product.option_groups.length === 1) {
       disableSingleSoldOptions(product);
+    }
+    if (product.option_groups.length > 1) {
+      findSoldOutOptionGroupValues(product);
     }
 
     $('.product_option_group').on('change',function() {
       disableAddButton();
       $('#option').val(0);
-      selected_values = getSelectedValues();
-      num_selected = selected_values.count(item => item > 0);
-      allSelected = selected_values.every(isGreaterThanZero);
-      if (allSelected) {
-        processAddToCartButton(product.options, selected_values);
-      }
+      processAvailableDropdownOptions(product);
     });
   }
+}
+
+function processAvailableDropdownOptions(product) {
+  selected_values = getSelectedValues();
+  num_selected = selected_values.count(item => item > 0);
+  allSelected = selected_values.every(isGreaterThanZero);
+  num_option_groups = product.option_groups.length;
+  if (allSelected) {
+    processAddToCartButton(product.options, selected_values);
+  }
+}
+
+
+function findSoldOutOptionGroupValues(product) {
+  product_option_groups = product.option_groups;
+  for (var POGIndex = 0; POGIndex < product_option_groups.length; POGIndex ++) {
+    product_option_group_values = product_option_groups[POGIndex].values;
+    for (var POGVIndex = 0; POGVIndex < product_option_group_values.length; POGVIndex ++) {
+      product_option_group_value = product_option_group_values[POGVIndex];
+      product_option_group_value_id = product_option_group_value.id
+      is_sold_out = checkIfAllValuesAreSoldOut(product.options,product_option_group_value_id);
+      if (is_sold_out) {
+        disableSelectOption($('.product_option_group').find('option[value="' + product_option_group_value_id + '"]'));
+      }
+    }
+  };
+}
+
+function checkIfAllValuesAreSoldOut(product_options, product_option_group_value_id) {
+  all_sold_out = true;
+  for (var productOptionsIndex = 0; productOptionsIndex < product_options.length; productOptionsIndex ++) {
+    option_group_values = product_options[productOptionsIndex].option_group_values;
+    for (var OptionGroupValuesIndex = 0; OptionGroupValuesIndex < option_group_values.length; OptionGroupValuesIndex ++) {
+      option_group_value = option_group_values[OptionGroupValuesIndex];
+      if (option_group_value.id === product_option_group_value_id) {
+        if (!product_options[productOptionsIndex].sold_out) {
+          all_sold_out = false;
+        }
+      }
+    }
+  };
+  return all_sold_out;
 }
 
 function processAddToCartButton(product_options, selected_values) {
@@ -24,7 +64,7 @@ function processAddToCartButton(product_options, selected_values) {
   if (product_option) {
     matching_price = product_option.price;
     sold_out = product_option.sold_out;
-    if (!sold_out) {
+    if (!sold_out && product_option.id > 0) {
       $('#option').val(product_option.id);
       enableAddButton(product_option.price);
     }
@@ -38,18 +78,21 @@ function processAddToCartButton(product_options, selected_values) {
 }
 
 function disableSingleSoldOptions(product) {
-  var product_options = product.options;
-  var product_option_groups = product.option_groups;
+  product_options = product.options;
+  product_option_groups = product.option_groups;
   for (var POGIndex = 0; POGIndex < product_option_groups.length; POGIndex ++) {
-    var product_option_group = product_option_groups[POGIndex];
-    var select = $(".product_option_group[name='option_group[" + product_option_group.id + "]']");
-    var sold_text = select.attr("data-sold-text");
-    select.find('option').each(function(index,element) {
-      if (element.value > 0) {
-        var matching_option = findProductOptionBySingleValue(product_options, element.value);
-        if (matching_option.sold_out) {
-          $(element).attr("disabled",true);
-          $(element).text(element.text + ' ' + sold_text);
+    product_option_group = product_option_groups[POGIndex];
+    select = $(".product_option_group[name='option_group[" + product_option_group.id + "]']");
+    select.find('option').each(function(index,select_option) {
+      if (select_option.value > 0) {
+        matching_option = findProductOptionBySingleValue(product_options, select_option.value);
+        if (matching_option) {
+          if (matching_option.sold_out) {
+            disableSelectOption($(select_option));
+          }
+        }
+        else {
+          $(select_option).remove();
         }
       }
     });
@@ -58,9 +101,9 @@ function disableSingleSoldOptions(product) {
 
 function findProductOptionBySingleValue(product_options, value) {
   for (var POIndex = 0; POIndex < product_options.length; POIndex ++) {
-    var group_values = product_options[POIndex].option_group_values;
+    group_values = product_options[POIndex].option_group_values;
     for (var GVIndex = 0; GVIndex < group_values.length; GVIndex ++) {
-      var group_value = group_values[GVIndex];
+      group_value = group_values[GVIndex];
       if (group_value.id == value) {
         return product_options[POIndex];
       }
@@ -70,8 +113,8 @@ function findProductOptionBySingleValue(product_options, value) {
 
 function findProductOptionBySelectedValues(product_options, selected_values) {
   for (var productOptionsIndex = 0; productOptionsIndex < product_options.length; productOptionsIndex ++) {
-    var option_group_values = product_options[productOptionsIndex].option_group_values;
-    var option_ids = [];
+    option_group_values = product_options[productOptionsIndex].option_group_values;
+    option_ids = [];
     option_group_values.forEach(function(option_group_value) {
       option_ids.push(option_group_value.id);
     });
@@ -82,7 +125,7 @@ function findProductOptionBySelectedValues(product_options, selected_values) {
 }
 
 function getSelectedValues() {
-  var selected_values = [];
+  selected_values = [];
   $('.product_option_group').each(function(i, object) {
     selected_values.push(object.value);
   });
