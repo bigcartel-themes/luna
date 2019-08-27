23518941bd108ca84641e9eb84b17e5dd7ecaaa5
Nick Endle <nick.endle@gmail.com>
2019-08-27 14:11:24 -0500

    Better browser support for dsiabled options
    This change wraps the disabled options in a span tag to hide them from all browsers and formats visible disabled options in a nicer way for Firefox

--- source/javascripts/store.js
+++ source/javascripts/store.js
@@ -233,20 +233,35 @@ function enableSelectOption(select_option) {
   select_option.removeAttr("disabled");
   select_option.text(select_option.attr("data-name"));
   select_option.removeAttr("disabled-type");
+  if ((select_option.parent().is('span'))) {
+    select_option.unwrap();
+  }
 }
 function disableSelectOption(select_option, type) {
   if (type === "sold-out") {
     disabled_text = select_option.parent().attr("data-sold-text");
     disabled_type = "sold-out";
+    if (show_sold_out_product_options === 'false') {
+      hide_option = true;
+    }
+    else {
+      hide_option = false;
+    }
   }
   if (type === "unavailable") {
     disabled_text = select_option.parent().attr("data-unavailable-text");
     disabled_type = "unavailable";
+    hide_option = true;
   }
   if (select_option.val() > 0) {
     var name = select_option.attr("data-name");
     select_option.attr("disabled",true);
     select_option.text(name + ' ' + disabled_text);
     select_option.attr("disabled-type",disabled_type);
+    if (hide_option === true) {
+      if (!(select_option.parent().is('span'))) {
+        select_option.wrap('<span>');
+      }
+    }
   }
 }
