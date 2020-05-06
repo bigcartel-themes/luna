d8eb70a4dbf6009c8dc24bd18ef319090b420386
Nick Endle <nick.endle@gmail.com>
2020-05-06 17:06:00 -0500

    Add intersection observer + polyfill for lazy loading

--- source/javascripts/store.js
+++ source/javascripts/store.js
@@ -265,3 +265,26 @@ function disableSelectOption(select_option, type) {
     }
   }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  if ("IntersectionObserver" in window) {
+    let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+      entries.forEach(function(entry) {
+        if (entry.isIntersecting) {
+          let lazyImage = entry.target;
+          lazyImage.src = lazyImage.dataset.src;
+          lazyImage.srcset = lazyImage.dataset.srcset;
+          lazyImage.classList.remove("lazy");
+          lazyImageObserver.unobserve(lazyImage);
+        }
+      }, {
+        rootMargin: "0px 0px 256px 0px"
+      });
+    });
+
+    lazyImages.forEach(function(lazyImage) {
+      lazyImageObserver.observe(lazyImage);
+    });
+  }
+});
