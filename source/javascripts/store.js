4d62cedf037bacbecfdf439fb83c12e5cf9698e3
Nick Endle <nick@bigcartel.com>
2023-09-21 09:39:41 -0500

    Add tinycolor support for displaying shades of different theme colors

--- source/javascripts/store.js
+++ source/javascripts/store.js
@@ -1,13 +1,40 @@
 "use strict";
 
 document.addEventListener('DOMContentLoaded', function() {
-  document.body.classList.remove("preload");
 
   let contactFields = document.querySelectorAll('.contact-form input, .contact-form textarea');
   contactFields.forEach(function(contactField) {
     contactField.removeAttribute('tabindex');
   });
+  const numShades = 5;
+
+  let cssProperties = [];
+
+  for (const themeColor in themeColors) {
+    const hexValue = themeColors[themeColor];
+    var hsl = tinycolor(hexValue).toHsl();
+    for (var i = numShades - 1; i >= 0; i -= 1) {
+      hsl.l = (i + 0.5) / numShades;
+      cssProperties.push(`--${camelCaseToDash(themeColor)}-${i * 100 / 1000 * 200}: ${tinycolor(hsl).toRgbString()};`)
+    }
+    numColor = tinycolor(hexValue).toRgb();
+    cssProperties.push(`--${camelCaseToDash(themeColor)}-rgb: ${numColor['r']}, ${numColor['g']}, ${numColor['b']};`)
+  }
+
+  const headTag = document.getElementsByTagName('head')[0];
+  const styleTag = document.createElement("style");
+
+  styleTag.innerHTML = `
+    :root {
+      ${cssProperties.join('\n')}
+    }
+  `;
+  headTag.appendChild(styleTag);
 });
+
+function camelCaseToDash(string) {
+  return string.replace(/([a-z])([A-Z])/g, '$1-$2').toLowerCase();
+}
 
 window.addEventListener("load", () => {
   document.body.classList.remove("transition-preload");
