659cad45c0b54b8291f76eebeeaf35730f6689d0
Trevin Chow <trevin@trevinchow.com>
2025-04-30 17:27:21 -0700

    feat: improve money format options for international currency support
    - Improves currency formatting for international support
    - Replaces `Format.money` with a new `formatMoney` function to enhance flexibility and better support international currency settings through usage of `Intl.NumberFormat()` over our legacy javascript api
    -  Introduces new `money_format` settings to use only currency code or show neither sign or code, while eliminating `sign_and_code`
    - Refactors related scripts and settings to align with the new implementation.

--- source/javascripts/cart.js
+++ source/javascripts/cart.js
@@ -97,7 +97,7 @@ function updateShareableLink() {
 }
 
 var processUpdate = function(input, item_id, new_val, cart) {
-  var sub_total = strip_tags(Format.money(cart.total, true, true));
+  var sub_total = strip_tags(formatMoney(cart.total, true, true));
   var item_count = cart.item_count;
 
   $('.cart-subtotal-amount').fadeOut(100, function() {
@@ -124,7 +124,7 @@ var processUpdate = function(input, item_id, new_val, cart) {
     for (itemIndex = 0; itemIndex < cart.items.length; itemIndex++) {
       if (cart.items[itemIndex].id == item_id) {
         item_price = cart.items[itemIndex].price;
-        formatted_item_price = strip_tags(Format.money(item_price, true, true));
+        formatted_item_price = strip_tags(formatMoney(item_price, true, true));
         item_price_element = $('.cart-item[data-item-id="'+item_id+'"]').find('.cart-item-details-price');
         item_price_element.fadeOut(100, function() {
           item_price_element.html(formatted_item_price);
